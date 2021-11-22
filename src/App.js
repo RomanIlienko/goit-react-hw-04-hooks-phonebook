9c8a5cef0d5dd5cd67e8f68b5cb365e8cc09c344
RomanIlienko <75617889+RomanIlienko@users.noreply.github.com>
2021-11-22 15:27:01 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,87 +1,166 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
+// import TodoContact from './components/TodoContact';
 import TodoContact from './components/TodoContact';
 import ContactsList from './components/ContactsList';
 import Filter from './components/Filter';
 
 import shortid from 'shortid';
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? [];
+  })
+  const [filter, setFilter] = useState('')
 
-  componentDidMount() {
-    const localContacts = localStorage.getItem('contacts')
-    const parsedContacts = JSON.parse(localContacts)
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts))
+  }, [contacts])
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts })
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    }
-  }
-
-  addContact = (name, number) => {
-    const todo = {
+  const addContact = (name, number) => {
+    const contact = {
       id: shortid.generate(),
       name,
       number,
     };
-    const { contacts } = this.state;
+    
+
     const alreadyInContacts = contacts.find(
-      contact => contact.name === todo.name && contact.number === todo.number,
+      contact => contact.name === name && contact.number === number,
     );
+
     if (alreadyInContacts) {
       alert(`${alreadyInContacts.name} is already in contacts.`);
+      
       return;
     }
-    if (todo.name === '') {
+    if (contact.name === '') {
       alert('Please enter data');
+
       return
     }
-    if (todo.number === '') {
+    if (contact.number === '') {
       alert('Please enter the number')
+
       return
     }
-    this.setState(prevState => ({
-      contacts: [todo, ...prevState.contacts],
-    }));
-  };
+    else {
+      setContacts(prevState => [
+        ...prevState, contact]
+      );
+    }
+      
+    };
 
-  ChangeFilter = e => {
-    const { value } = e.currentTarget;
+    const ChangeFilter = e => {
+      const { value } = e.currentTarget;
 
-    this.setState({ filter: value });
-  };
+      setFilter(value)
+    };
 
-  deleteContact = todoId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(todo => todo.id !== todoId),
-    }));
-  };
+    const deleteContact = id => {
+      setContacts(prevState => ({
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      }));
+    };
 
-  render() {
-    const normalizedFilter = this.state.filter.toLowerCase();
-    const visibleTodos = this.state.contacts.filter(todo =>
+  
+    const normalizedFilter = filter.toLowerCase();
+    const visibleContacts = contacts.filter(todo =>
       todo.name.toLowerCase().includes(normalizedFilter),
     );
 
+
     return (
       <>
-        <TodoContact onSubmit={this.addContact} />
-        <Filter value={this.state.filter} onChange={this.ChangeFilter} />
+        <TodoContact onSubmit={addContact} />
+        <Filter value={filter} onChange={ChangeFilter} />
         <ContactsList
-          contacts={visibleTodos}
-          onDeleteContact={this.deleteContact}
+          contacts={visibleContacts}
+          onDeleteContact={deleteContact}
         />
       </>
-    );
+    )
   }
-}
 
-export default App;
+export default App
+// class App extends Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
+
+//   componentDidMount() {
+//     const localContacts = localStorage.getItem('contacts')
+//     const parsedContacts = JSON.parse(localContacts)
+
+//     if (parsedContacts) {
+//       this.setState({ contacts: parsedContacts })
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.contacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+//     }
+//   }
+
+//   addContact = (name, number) => {
+//     const todo = {
+//       id: shortid.generate(),
+//       name,
+//       number,
+//     };
+//     const { contacts } = this.state;
+//     const alreadyInContacts = contacts.find(
+//       contact => contact.name === todo.name && contact.number === todo.number,
+//     );
+//     if (alreadyInContacts) {
+//       alert(`${alreadyInContacts.name} is already in contacts.`);
+//       return;
+//     }
+//     if (todo.name === '') {
+//       alert('Please enter data');
+//       return
+//     }
+//     if (todo.number === '') {
+//       alert('Please enter the number')
+//       return
+//     }
+//     this.setState(prevState => ({
+//       contacts: [todo, ...prevState.contacts],
+//     }));
+//   };
+
+//   ChangeFilter = e => {
+//     const { value } = e.currentTarget;
+
+//     this.setState({ filter: value });
+//   };
+
+//   deleteContact = todoId => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(todo => todo.id !== todoId),
+//     }));
+//   };
+
+//   render() {
+//     const normalizedFilter = this.state.filter.toLowerCase();
+//     const visibleTodos = this.state.contacts.filter(todo =>
+//       todo.name.toLowerCase().includes(normalizedFilter),
+//     );
+
+//     return (
+//       <>
+//         {/* <TodoContact onSubmit={this.addContact} /> */}
+//         <TodoContacts onSubmit={this.addContact} />
+//         <Filter value={this.state.filter} onChange={this.ChangeFilter} />
+//         <ContactsList
+//           contacts={visibleTodos}
+//           onDeleteContact={this.deleteContact}
+//         />
+//       </>
+//     );
+//   }
+// }
+
+
--- src/components/TodoContact/TodoContact.js
+++ src/components/TodoContact/TodoContact.js
@@ -1,54 +1,63 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import s from './TodoContact.module.css';
-// import shortid from 'shortid';
+import PropTypes from "prop-types";
 
-class TodoContact extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+function TodoContact({ onSubmit }) {
+    const [name,setName] = useState('') 
+    const [number, setNumber] = useState('')
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    
-    this.setState({ [name]: value });
-  };
+    const handleChange = event => {
+        const { name, value } = event.currentTarget
+        switch (name) {
+            case 'name':
+                setName(value);
+                break;
+            case 'number':
+                setNumber(value);
+                break;
+            default:
+                return;
+        }
+    }
 
-  handleSubmit = e => {
-    e.preventDefault();
 
-    this.props.onSubmit(this.state.name, this.state.number);
+    const handleSubmit = e => {
+      e.preventDefault();
+      onSubmit(name, number);
+      reset();
+    };
 
-    this.reset();
-  };
+    const reset = () => {
+        setName('');
+        setNumber('');
+    };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
-
-  render() {
+  
     return (
       <div>
         <div className={s.Wrapper}>
           <h1 className={s.titleText}>Phonebook</h1>
         </div>
-        <form onSubmit={this.handleSubmit} className={s.headStyle}>
+        <form onSubmit={handleSubmit} className={s.headStyle}>
           <label className={s.labelStyle}>
             Name
             <input
               className={s.inputStyle}
+              type='text'
               name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
+              value={name}
+              onChange={handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             />
           </label>
           <label className={s.labelStyle}>
             Number
             <input
               className={s.inputStyle}
+              type="tel"
               name="number"
-              value={this.state.number}
-              onChange={this.handleChange}
+              value={number}
+              onChange={handleChange}
             />
           </label>
           <button type="submit" className={s.button}>
@@ -57,7 +66,69 @@ class TodoContact extends Component {
         </form>
       </div>
     );
-  }
+}
+
+TodoContact.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
 }
 
 export default TodoContact;
+
+// class TodoContact extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   handleChange = e => {
+//     const { name, value } = e.currentTarget;
+    
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     this.props.onSubmit(this.state.name, this.state.number);
+
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({ name: '', number: '' });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <div className={s.Wrapper}>
+//           <h1 className={s.titleText}>Phonebook</h1>
+//         </div>
+//         <form onSubmit={this.handleSubmit} className={s.headStyle}>
+//           <label className={s.labelStyle}>
+//             Name
+//             <input
+//               className={s.inputStyle}
+//               name="name"
+//               value={this.state.name}
+//               onChange={this.handleChange}
+//             />
+//           </label>
+//           <label className={s.labelStyle}>
+//             Number
+//             <input
+//               className={s.inputStyle}
+//               name="number"
+//               value={this.state.number}
+//               onChange={this.handleChange}
+//             />
+//           </label>
+//           <button type="submit" className={s.button}>
+//             Add contact
+//           </button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
